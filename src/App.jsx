b1695067f8bb690bd5b27c346f3508cc9f846b6c
Tsitohaina <sitrakatsitohainarajoelisolo@gmail.com>
2023-12-10 20:18:06 +0100

    version app

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,32 @@
-import './bulma.css';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faFacebook } from '@fortawesome/free-solid-svg-icons';
+import Header from './component/1-header/Header';
+import Author from './component/2-author-book/Author';
+import About from './component/3-About/About';
+import Trusted from './component/4-trusted-by/Trusted';
+import Call0 from './component/5-callout0/Call0';
+import What from './component/6-what-will-you-learn/What';
+import Chapter from './component/7-the-chapter/Chapter';
+import Testimonial from './component/8-testimonial/Testitmonial';
+import Blog from './component/9-blog/Blog';
+import Call1 from './component/10-callout1/Call1';
+import Footer from './component/11-footer/Footer';
 
-library.add(faFacebook);
+import  facebook  from './svg/facebook.svg'
 
 function App() {
   return (
     <div>
-        <nav className="navbar custom-navbar has-align-items-centered" id='colorBack'>
-          <div className="navbar-brand">
-              <a className="navbar-item navbar-logo" href="#">
-                <img src="./image/Logo.png" className='mt-1' width="19.5" height='23' alt="" />
-                <h1 className='is-size-4'>Pages</h1>
-              </a>
-
-              <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </a>
-          </div>
-
-          <div id="navbarBasicExample" className="navbar-menu">
-             <div className="navbar-start">
-              <a className="navbar-item" href="#">
-                 <FontAwesomeIcon icon={faFacebook} />
-              </a>
-
-              <a className="navbar-item" href="#">
-                À propos
-              </a>
-
-              <a className="navbar-item" href="#">
-                Services
-              </a>
-            </div>
-
-            <div className="navbar-end navbar-custom-end">
-              <div className="navbar-item">
-                  <div className="buttons">
-                     <a className="button is-primary">
-                        <strong>S'inscrire</strong>
-                     </a>
-                      <a className="button is-light">
-                         Se connecter
-                      </a>
-                  </div>
-              </div>
-            </div>
-          </div>
-      </nav>
+        <Header/>
+        <Author/>
+        <About/>
+        <Trusted/>
+        <Call0/>
+        <What/>
+        <Chapter/>
+        <Testimonial/>
+        <Blog/>
+        <Call1/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/component/10-callout1/Call1.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Call1() {
+  return (
+    <div>Call1</div>
+  )
+}
